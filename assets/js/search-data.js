eda8d0d010dee8149273be250e0f80a0d06f179e
gd32 <georgedewey32@gmail.com>
2025-07-15 18:36:52 +0000

    Deploying to gh-pages from @ gd32/gd32.github.io@4d0f6328c3e42915ca78b983699c3221025c934f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -391,16 +391,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-i-gave-a-talk-entitled-epidemic-insights-from-time-varying-signals-at-the-center-for-communicable-disease-dynamics-at-the-harvard-th-chan-school-of-public-health",
+          title: 'I gave a talk entitled “Epidemic Insights from Time-Varying Signals” at the Center...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
